ca13862ceef76e6b1e404a3c09e64875dd2e8df2
Amr Elnaggar <amr.eln2gar@gmail.com>
2020-10-12 17:29:41 +0200

    Implemented express

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,16 +1,27 @@
 
-import React from 'react'
-import products from '../products'
+import React, { useState, useEffect } from 'react'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
+import axios from 'axios'
+
 
 const HomeScreen = () => {
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const { data } = await axios.get('/api/products')
+            setProducts(data)
+        }
+
+        fetchProducts()
+    }, [])
     return (
         <>
             <h1>Latest Products</h1>
             <Row>
                 {products.map(product => (
-                    <Col md={6} ld={4} xl={3}>
+                    <Col key={product._id} md={6} ld={4} xl={3}>
                         <Product key={product._id} product={product} />
                     </Col>
                 ))}
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
-
+import axios from 'axios'
 
 const ProductScreen = ({ match }) => {
-    const product = products.find(p => p._id === match.params.id)
+    const [product, setProduct] = useState({})
+
+    useEffect(() => {
+        const fetchProduct = async () => {
+            const { data } = await axios.get(`/api/products/${match.params.id}`)
+            setProduct(data)
+        }
+        fetchProduct()
+    })
 
     return (
         <>
@@ -14,7 +21,7 @@ const ProductScreen = ({ match }) => {
             <Row>
                 <Col md={6}>
                     <h3>{product.name}</h3>
-                    <Image src={product.image} alt={product.name} alt={product.name} fluid />
+                    <Image src={product.image} alt={product.name} fluid />
                 </Col>
                 <Col md={3}>
                     <ListGroup variant='flush'>
