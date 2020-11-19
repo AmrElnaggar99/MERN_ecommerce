ef1b4882dcaa9d7ebd4cf2202e758060eeda116a
Amr Elnaggar <amr.eln2gar@gmail.com>
2020-11-20 00:13:49 +0100

    implemented the orders screen

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import { protect } from '../middleware/authMiddleware.js'
-import { addOrderItems, getOrderById, updateOrderToPaid } from '../controllers/orderController.js'
+import { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders } from '../controllers/orderController.js'
 const router = express.Router()
 
 import { authUser, getUserProfile, registerUser, updateUserProfile } from '../controllers/userControllers.js'
 
 router.route('/').post(protect, addOrderItems)
+router.route('/myorders').get(protect, getMyOrders)
 router.route('/:id').get(protect, getOrderById)
-router.route('/:id/pay').get(protect, updateOrderToPaid)
+router.route('/:id/pay').put(protect, updateOrderToPaid)
+
 
 export default router
--- frontend/src/screens/orderScreen.js
+++ frontend/src/screens/orderScreen.js
@@ -1,13 +1,18 @@
-import React, { useEffect } from 'react'
-import { Button, Row, Col, ListGroup, Image, Card, ListGroupItem } from 'react-bootstrap'
+import React, { useState, useEffect } from 'react'
+import { Row, Col, ListGroup, Image, Card, ListGroupItem } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
 import { Link } from 'react-router-dom'
-import { getOrderDetails } from '../actions/orderActions'
+import { getOrderDetails, payOrder } from '../actions/orderActions'
+import axios from 'axios'
+import { PayPalButton } from 'react-paypal-button-v2'
+import {ORDER_PAY_RESET} from '../constants/orderConstants'
 
 const OrderScreen = ({match}) => {
     const orderId = match.params.id
+
+    const [sdkReady, setSDKReady] = useState(false)
 
     const dispatch = useDispatch()
     
@@ -15,6 +20,9 @@ const OrderScreen = ({match}) => {
 
     const orderDetails = useSelector((state) => state.orderDetails)
     const { order, loading, error } = orderDetails
+
+    const orderPay = useSelector((state) => state.orderPay)
+    const {loading: loadingPay, success:successPay } = orderPay
 if(!loading){
     // calculate prices
     const addDecimals = (num) => {
@@ -24,11 +32,35 @@ if(!loading){
     order.itemsPrice = addDecimals(order.orderItems.reduce((acc, item) => acc + item.price * item.qty, 0))
 }
     useEffect(() => {
-        if(!order || order._id !== orderId){
-            dispatch(getOrderDetails(orderId))
+        const addPayPalScript = async () => {
+            const {data : clientId } = await axios.get('/api/config/paypal')
+            const script = document.createElement('script')
+            script.type = 'text/javascript'
+            script.async = true
+            script.src = `https://www.paypal.com/sdk/js?client-id=${clientId}`
+            script.onload = () => {
+                setSDKReady(true)
+            }
+
+            document.body.appendChild(script)
         }
-    }, [dispatch, orderId])
+
+        if( !order || successPay || order._id !== orderId ){
+            dispatch({type: ORDER_PAY_RESET})
+            dispatch(getOrderDetails(orderId))
+        } else if (!order.isPaid ){
+            if(!window.paypal){
+                addPayPalScript()
+            } else {
+                setSDKReady(true)
+            }
+        }
+    }, [dispatch, successPay, orderId, order])
     
+    const successPaymentHandler = (paymentResult) => {
+        console.log(paymentResult)
+        dispatch(payOrder(orderId, paymentResult))
+    }
     return (
     loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> :
     <>
@@ -118,7 +150,15 @@ if(!loading){
                                     <Col>${order.totalPrice}</Col>
                                 </Row>
                             </ListGroup.Item>
-                            
+                            {!order.isPaid && (
+                                <ListGroup.Item>
+                                    {loadingPay && <Loader />}
+                                    {!sdkReady ? <Loader /> : (
+                                        <PayPalButton amount={order.totalPrice} onSuccess={successPaymentHandler}></PayPalButton>
+                                    )}
+
+                                </ListGroup.Item>
+                            )}
                         </ListGroup>
                     </Card>
                 </Col>
